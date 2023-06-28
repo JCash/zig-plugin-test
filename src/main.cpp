33f696d3a9848bd7cbdbac971d6403eaba163a85
JCash <mwesterdahl76@gmail.com>
2023-06-29 01:20:43 +0200

    Added Lua support to zig plugin

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,16 @@ extern "C" void theLinkerTrick()
     PluginZIG();
 }
 
+static bool RunString(lua_State* L, const char* script)
+{
+    if (luaL_dostring(L, script) != 0)
+    {
+        printf("Error: %s", lua_tolstring(L, -1, 0));
+        return false;
+    }
+    return true;
+}
+
 int main(int argc, char** argv)
 {
     printf("main.cpp: Entering\n");
@@ -32,6 +42,20 @@ int main(int argc, char** argv)
     {
         plugin_updatePlugins();
     }
+
+    // Call some Lua script
+    printf("\nmain.cpp: Run lua script\n\n");
+    const char* script = "\n"
+    "local s = 'Hello World!'\n" \
+    "print('String: ', s)\n" \
+    "local e = plugincpp.encode(s)\n" \
+    "print('Encoded (C++):', e)\n" \
+    "local d = pluginzig.decode(e)\n" \
+    "print('Decoded (Zig):', d)\n" \
+    ;
+    RunString(L, script);
+
+    printf("\nmain.cpp: End script\n\n");
 
     plugin_destroyPlugins();
 
--- src/plugin.zig
+++ src/plugin.zig
@@ -3,46 +3,69 @@ const print = std.debug.print;
 
 
 const c = @cImport({
-    @cInclude("lua.h");
-    // @cInclude("lualib.h");
-    // @cInclude("lauxlib.h");
+    @cInclude("lauxlib.h");
     @cInclude("./src/plugin.h");
-    @cInclude("./src/util.h");
 });
 
 const ZigPluginContext = struct {
     some_value: i32,
 };
 
+fn Decode(L: ?*c.lua_State) callconv(.C) i32 {
+    var msglen : usize = 0;
+    var msg : [*c]const u8 = c.luaL_checklstring(L, 1, &msglen);
 
-fn zigPluginCreate(name: [*c]const u8, L: ?*c.lua_State) callconv(.C) ?*anyopaque {
-    print("    PluginZIG: zigPluginCreate {s} {*}\n", .{name, L});
+    const t : []u8 = std.heap.c_allocator.alloc(u8, msglen) catch "";
+    std.mem.copy(u8, t, msg[0..msglen]);
+
+    var i : usize = 0;
+    while (i < msglen) : (i += 1) {
+        t[i] -= 1;
+    }
+
+    c.lua_pushstring(L, t.ptr);
+    return 1;
+}
+
+const Module_methods = [_]c.luaL_Reg{
+    .{.name = "decode", .func = &Decode},
+    .{.name = 0, .func = null}
+};
+
+fn RegisterModule(L: ?*c.lua_State) void {
+    c.luaL_register(L, "pluginzig", &Module_methods[0]);
+    c.lua_pop(L, 1);
+}
+
+fn PluginCreate(name: [*c]const u8, L: ?*c.lua_State) callconv(.C) ?*anyopaque {
+    print("    PluginZIG: PluginCreate {s} {*}\n", .{name, L});
     const ctx : *ZigPluginContext = std.heap.c_allocator.create(ZigPluginContext) catch return null;
     ctx.some_value = 0;
+    RegisterModule(L);
     return @ptrCast(*anyopaque, ctx);
 }
 
-fn zigPluginDestroy(_plugin: ?*anyopaque) callconv(.C) void {
+fn PluginDestroy(_plugin: ?*anyopaque) callconv(.C) void {
     const plugin : *ZigPluginContext = @ptrCast(*ZigPluginContext, @alignCast(4, _plugin));
-    print("    PluginZIG: zigPluginDestroy {*}\n", .{plugin});
+    print("    PluginZIG: PluginDestroy {*}\n", .{plugin});
     std.heap.c_allocator.destroy(plugin);
 }
 
-fn zigPluginUpdate(_plugin: ?*anyopaque) callconv(.C) void {
+fn PluginUpdate(_plugin: ?*anyopaque) callconv(.C) void {
     const plugin : *ZigPluginContext = @ptrCast(*ZigPluginContext, @alignCast(4, _plugin));
     plugin.some_value += 1;
-    print("    PluginZIG: zigPluginUpdate {}\n", .{plugin.some_value});
+    print("    PluginZIG: PluginUpdate {}\n", .{plugin.some_value});
 }
 
-
-fn allocatePlugin() ?*c.Plugin {
+fn AllocatePlugin() ?*c.Plugin {
     var plugin : *c.Plugin = std.heap.c_allocator.create(c.Plugin) catch return null;
     return plugin;
 }
 
 export fn PluginZIG() callconv(.C) void {
-    const plugin : ?*c.Plugin = allocatePlugin();
-    c.plugin_registerPlugin(plugin, "PluginZIG", zigPluginCreate, zigPluginDestroy, zigPluginUpdate);
+    //const plugin : ?*c.Plugin = std.heap.c_allocator.create(c.Plugin) catch return null;
+    const plugin : ?*c.Plugin = AllocatePlugin();
+    c.plugin_registerPlugin(plugin, "PluginZIG", PluginCreate, PluginDestroy, PluginUpdate);
 }
 
 export const mod_init_func_ptr linksection("__DATA,__mod_init_func") = &PluginZIG; // note the & op
